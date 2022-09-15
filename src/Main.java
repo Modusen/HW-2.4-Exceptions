c346aaef021cbce90af2144ee8dd8dcc43a8d89e
astra <diman.lan@Mail.ru>
2022-09-15 11:20:26 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String login = "Lisk149feoa12k11112";
+        String login = "Lisk1w9fe_oa12k112";
         String password = "C1diWAgo2k_eg2u4S11";
         String confirmPassword = "C1diWAgo2k_eg2u4S11";
         boolean result = checkAuthorization(login, password, confirmPassword);
@@ -14,10 +14,19 @@ public class Main {
     public static boolean checkAuthorization(String login, String password, String confirmPassword) {
         int exceptionCounter = 0;
         boolean result;
+
         try {
+            if (!login.matches("[a-zA-Z_0-9]+")) {
+                exceptionCounter++;
+                throw new WrongSyntaxException();
+            }
             if (login.length() > 20) {
                 exceptionCounter++;
                 throw new WrongLoginException();
+            }
+            if (!password.matches("[a-zA-Z_0-9]+")) {
+                exceptionCounter++;
+                throw new WrongSyntaxException();
             }
             if (password.length() > 19) {
                 exceptionCounter++;
@@ -27,6 +36,8 @@ public class Main {
                 exceptionCounter++;
                 throw new WrongPasswordException();
             }
+        } catch (WrongSyntaxException e) {
+            System.out.println("Введены недопустимые символы");
         } catch (WrongLoginException e) {
             System.out.println("Длина логина слишком велика");
         } catch (WrongPasswordLengthException e) {
---
+++ src/WrongSyntaxException.java
@@ -0,0 +1,2 @@
+public class WrongSyntaxException extends Exception{
+}
